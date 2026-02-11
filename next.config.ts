e96d6a09fb62b47dbf034f8068235c55b440b24b
Vankayalapati22 <balaraju.vankayalapati@winfosolutions.com>
2026-02-11 12:13:16 +0530

    ..

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: "export",
+  basePath: "/music-hub",
+  assetPrefix: "/music-hub/",
+  images: {
+    unoptimized: true,
+  },
 
-export default nextConfig;
+
+};
+module.exports = nextConfig;
