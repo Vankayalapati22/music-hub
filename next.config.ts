4cbf12c01dccc96c58d028f6856bb063a2b59184
Vankayalapati22 <balaraju.vankayalapati@winfosolutions.com>
2026-02-12 11:56:18 +0530

    versionchange

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/music-hub",
-  assetPrefix: "/music-hub/",
+  basePath: "/music-hub",//navigation (my app is Not running at root / It is running inside a folder )
+  assetPrefix: "/music-hub/", //(Load CSS,JS,Images from this folder)
   images: {
     unoptimized: true,
   },
